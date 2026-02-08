1c6c0e2dd3ce569d8099b5c8fe5f976351b80c89
cmelo <m.celin@gmail.com>
2026-02-08 18:45:06 +0100

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { FooterComponent } from '@shared/components/footer/footer.component';
 import { HeaderComponent } from '@shared/components/header/header.component';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
-	imports: [RouterOutlet, HeaderComponent],
+	imports: [RouterOutlet, HeaderComponent, FooterComponent],
 })
 export class AppComponent {}
---
+++ src/shared/components/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'c-footer',
+	imports: [],
+	templateUrl: './footer.component.html',
+	styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
